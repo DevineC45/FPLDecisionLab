8055fbf8f7c96b78816096c31fac3d55eb705464
DevineC45 <conordevine11@icloud.com>
2026-09-13 21:25:43 +0100

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+/****
+***** Created by Conor on 13/09/2026
+***** UPDATE PROGRAM DESCRIPTION HERE
+****/public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
